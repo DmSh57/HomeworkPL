691f60cfed886ab71a3a679bd57b66bacd5311f5
Dmitry <Shapovalov.D.97@yandex.ru>
2026-04-07 21:29:12 +0300

    HW_STROKA

--- src/homework7/Homework7_5.java
+++ src/homework7/Homework7_5.java
@@ -1,9 +1,14 @@
 package homework7;
 
+import java.util.Scanner;
+
 public class Homework7_5 {
     public static void main(String[] args) {
-        String text = "Hello";
+
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
         String result = text.replaceAll(".", "$0$0");
         System.out.println(result);
+        scanner.close();
     }
 }
